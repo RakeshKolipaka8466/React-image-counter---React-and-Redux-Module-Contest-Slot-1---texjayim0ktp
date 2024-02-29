00c3e4b21c46d1154dad4e9b5044244277a1e463
Rakesh <134631155+RakeshKolipaka8466@users.noreply.github.com>
2024-02-29 22:21:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,19 @@ import React, { useState } from 'react'
 import '../styles/App.css';
 import star from '../star.png'
 const App = () => {
+  const [width,setWidth]=useState(300);
+  const [length,setLength]=useState(300);
+  const changeArea=()=>{
+    setWidth(width+2);
+    setLength(length+2);
+  }
   return (
     <div id="main">
-      <img src={star}  height="300px" width="300px" />
+      <img src={star}  onClick={changeArea} height={`${length}px`} width={`${width}px`} />
     </div>
   )
 }
 
 
 export default App;
+
